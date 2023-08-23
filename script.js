68a7f549894422c89961599067108413ede76770
rncraven94 <neilcraven2013@gmail.com>
2023-08-23 09:43:32 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 use: "strict";
+
+const btn = document.querySelector(".btn-confirm");
+
+const request = new XMLHttpRequest();
+request.open("GET", "https://api.weather.gov/openapi.json");
+request.send();
+
+request.addEventListener("load", function () {
+  console.log(this.responseText);
+});
