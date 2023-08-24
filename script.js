646378951161f1c857d4e5566b4dfa2027b9d235
rncraven94 <neilcraven2013@gmail.com>
2023-08-24 19:49:41 -0400

    script.js

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ use: "strict";
 
 const btn = document.querySelector(".btn-confirm");
 
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+  },
+  function () {
+    alert("Could not get your position");
+  }
+);
+
 const request = new XMLHttpRequest();
 request.open("GET", "https://api.weather.gov/points/39.7456,-97.0892");
 request.send();
