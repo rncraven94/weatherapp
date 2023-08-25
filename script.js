22b6a38db08bd8134fc7273bde655b58a4a9cdea
rncraven94 <neilcraven2013@gmail.com>
2023-08-24 21:35:20 -0400

    script.js

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
 use: "strict";
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.querySelector(".btn-confirm");
+  const weatherContainer = document.getElementById("weather-container");
 
-const btn = document.querySelector(".btn-confirm");
-const weatherContainer = document.getElementById("weather-container");
-
-// checking the geolocation of the user for the weather
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(latitude, longitude);
-      const gridRequest = new XMLHttpRequest();
-      gridRequest.open(
-        "GET",
-        `https://api.weather.gov/points/${latitude},${longitude}`
-      );
-      gridRequest.send();
-      gridRequest.addEventListener("load", function () {
-        const data = JSON.parse(this.responseText);
-        const dataGridX = data.properties.gridX;
-        const dataGridY = data.properties.gridY;
-        console.log(dataGridX);
-        console.log(dataGridX, dataGridY);
-        const request = new XMLHttpRequest();
-        request.open(
+  // checking the geolocation of the user for the weather
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        const { latitude } = position.coords;
+        const { longitude } = position.coords;
+        console.log(latitude, longitude);
+        const gridRequest = new XMLHttpRequest();
+        gridRequest.open(
           "GET",
-          // `https://api.weather.gov/points/${latitude},${longitude}`,
-          `https://api.weather.gov/gridpoints/TOP/${dataGridX},${dataGridY}/forecast`
+          `https://api.weather.gov/points/${latitude},${longitude}`
         );
-        request.send();
-
-        request.addEventListener("load", function () {
-          //   console.log(this.responseText);
+        gridRequest.send();
+        gridRequest.addEventListener("load", function () {
           const data = JSON.parse(this.responseText);
-          const dataset = data.properties.periods;
-          //destructuring the data coming into the function
-          const formatedData = dataset.map(
-            //breaks the function into a more managable chunk
-            ({ name, temperature, windSpeed }) => ({
-              name,
-              temperature,
-              windSpeed,
-            })
+          const dataGridX = data.properties.gridX;
+          const dataGridY = data.properties.gridY;
+          console.log(dataGridX);
+          console.log(dataGridX, dataGridY);
+          const request = new XMLHttpRequest();
+          request.open(
+            "GET",
+            // `https://api.weather.gov/points/${latitude},${longitude}`,
+            `https://api.weather.gov/gridpoints/TOP/${dataGridX},${dataGridY}/forecast`
           );
-          //breaks down the list further
-          formatedData.forEach((entry, index) => {
-            console.log(`Entry ${index + 1}:`);
-            console.log(`Name: ${entry.name}`);
-            console.log(`Temperature: ${entry.temperature}`);
-            console.log(`Wind Speed: ${entry.windSpeed}`);
-            console.log("--------------------------");
-            const entryDiv = document.createElement("div");
-            entryDiv.classList.add("weather-container");
+          request.send();
 
-            const nameElement = document.createElement("p");
-            nameElement.textContent = `${entry.name}`;
-            entryDiv.appendChild(nameElement);
+          request.addEventListener("load", function () {
+            //   console.log(this.responseText);
+            const data = JSON.parse(this.responseText);
+            const dataset = data.properties.periods;
+            console.log(dataset);
+            //destructuring the data coming into the function
+            const formatedData = dataset.map(
+              //breaks the function into a more managable chunk
+              ({ name, temperature, detailedForecast, windSpeed }) => ({
+                name,
+                temperature,
+                detailedForecast,
+                windSpeed,
+              })
+            );
+            //breaks down the list further
+            formatedData.forEach((entry, index) => {
+              // console.log(`Entry ${index + 1}:`);
+              // console.log(`Name: ${entry.name}`);
+              // console.log(`Temperature: ${entry.temperature}`);
+              // console.log(`Wind Speed: ${entry.windSpeed}`);
+              // console.log("--------------------------");
+              const entryDiv = document.createElement("div");
+              entryDiv.classList.add("weather-container");
 
-            weatherContainer.appendChild(entryDiv);
+              const nameElement = document.createElement("p");
+              nameElement.textContent = `${entry.name}`;
+              entryDiv.appendChild(nameElement);
+
+              const tempElement = document.createElement("p");
+              tempElement.textContent = `${entry.temperature}°F`;
+              entryDiv.appendChild(tempElement);
+
+              const detailedElement = document.createElement("p");
+              detailedElement.textContent = `${entry.detailedForecast}`;
+              entryDiv.appendChild(detailedElement);
+
+              const windElement = document.createElement("p");
+              windElement.textContent = `${entry.windSpeed}`;
+              entryDiv.appendChild(windElement);
+
+              weatherContainer.appendChild(entryDiv);
+            });
           });
         });
-      });
-    },
-    function () {
-      alert("Could not get your position");
-    }
-  );
+      },
+      function () {
+        alert("Could not get your position");
+      }
+    );
+});
