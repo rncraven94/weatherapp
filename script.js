44e97837c2054f8e7938e684399524ad04c2dfdb
rncraven94 <neilcraven2013@gmail.com>
2023-08-24 20:27:13 -0400

    script.js

--- script.js
+++ script.js
@@ -2,21 +2,37 @@ use: "strict";
 
 const btn = document.querySelector(".btn-confirm");
 
-navigator.geolocation.getCurrentPosition(
-  function (position) {
-    console.log(position);
-  },
-  function () {
-    alert("Could not get your position");
-  }
-);
+// checking the geolocation of the user for the weather
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(latitude, longitude);
+      const request = new XMLHttpRequest();
+      request.open(
+        "GET",
+        // `https://api.weather.gov/points/${latitude},${longitude}`,
+        `https://api.weather.gov/gridpoints/TOP/31,80/forecast`
+      );
+      request.send();
 
-const request = new XMLHttpRequest();
-request.open("GET", "https://api.weather.gov/points/39.7456,-97.0892");
-request.send();
-
-request.addEventListener("load", function () {
-  //   console.log(this.responseText);
-  const data = JSON.parse(this.responseText);
-  console.log(data);
-});
+      request.addEventListener("load", function () {
+        //   console.log(this.responseText);
+        const data = JSON.parse(this.responseText);
+        const dataset = data.properties.periods;
+        //destructuring the data coming into the function
+        const formatedData = dataset.map(
+          ({ name, temperature, windSpeed }) => ({
+            name,
+            temperature,
+            windSpeed,
+          })
+        );
+        console.log(formatedData);
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
